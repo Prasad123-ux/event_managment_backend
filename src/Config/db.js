a396c47cf1bd92c195fe18cff7b88c86b0b1d271
prasad <prasadmetkar333@gmail.com>
2024-08-17 11:25:16 +0530

    change in database

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,10 +1,9 @@
 const mongoose= require("mongoose")
 const env = require("dotenv")
 env.config()
-// mongoose.connect(`mongodb://0.0.0.0/Event_Management_system`)
 
 
-mongoose.connect('mongodb+srv://prasadmetkar333:uWFPsDYkN1SOcu6i@cluster0.awfae.mongodb.net/event_managment?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.awfae.mongodb.net/event_managment?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     console.log("database connected")
 }).catch(()=>{
